161439ae2ce5397205ebcdca59e94f885342c650
gamari <gamari4552@gmail.com>
2023-09-13 06:20:35 +0900

    update

---
+++ src/components/base/hooks/useScrollPosition.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+export const useScrollPosition = () => {
+    const [scrollY, setScrollY] = useState(window.scrollY);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrollY(window.scrollY);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    return { scrollY };
+}
--- src/components/company2/components/Header/Header.tsx
+++ src/components/company2/components/Header/Header.tsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Logo } from "./Logo";
 import { Navigation } from "./Navigation";
+import { useScrollPosition } from "@/components/base/hooks/useScrollPosition";
 
 export const Header = () => {
+  const { scrollY } = useScrollPosition();
+
+  const isBeyond100vh = scrollY > window.innerHeight;
+
   return (
     <header
       className={`
         fixed flex justify-center flex-col
         w-full h-16
         px-4 md:px-6
-        text-white 
+        ${
+          isBeyond100vh
+            ? "z-20 border-b shadow bg-white text-black"
+            : "text-white"
+        }
       `}
     >
       <div className="flex flex-row justify-between items-center">
